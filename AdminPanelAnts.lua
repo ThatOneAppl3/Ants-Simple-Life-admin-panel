cd766bbf41ece6c86edbbb2a831460a84a266795
ThatOneAppl3 <luizafreitas1275@gmail.com>
2026-08-02 14:12:36 -0300

    Add bypass buttons and update window icon
    Updated the Admin Panel to include new buttons for bypassing anticheat and weather effects, and changed the window icon.

--- AdminPanelAnts.lua
+++ AdminPanelAnts.lua
@@ -4,9 +4,10 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 --█▀█ █▄▀ █░▀░█ █ █░▀█
 --█▀█ ▄▀█ █▄░█ █▀▀ █░
 --█▀▀ █▀█ █░▀█ ██▄ █▄
+
 local Window = Rayfield:CreateWindow({
    Name = "Ant Panel Premium",
-   Icon = 0,
+   Icon = "shield-check",
    LoadingTitle = "Ant Admin Panel",
    LoadingSubtitle = "Shitting hard or hardly shitting?",
    ShowText = "Admin Panel",
@@ -39,4 +40,64 @@ local Window = Rayfield:CreateWindow({
       GrabKeyFromSite = false,
       Key = {"Apple"}
    }
+})
+
+-- TABS TO USE
+
+local Main = Window:CreateTab("Main", "tool-case")
+
+-- Main tab
+
+local Label = Main:CreateLabel("Game", 4483362458, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, Ignoretheme?
+
+local Divider = Main:CreateDivider()
+
+local ButtonCheat = Main:CreateButton({
+   Name = "Bypass anticheat",
+   Callback = function()
+   local Event = game:GetService("ReplicatedStorage"):WaitForChild("Kickthem")
+
+local oldNamecall
+oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+
+    if self == Event and method == "FireServer" then
+        print("detected anticheat call, blocked")
+        return nil
+    end
+
+    return oldNamecall(self, ...)
+end))
+Rayfield:Notify({
+   Title = "Anticheat",
+   Content = "Walkspeed + Flying enabled permanently",
+   Duration = 3,
+   Image = 4483362458,
+})
+   end,
+})
+
+local ButtonWeather = Main:CreateButton({
+   Name = "Bypass Weather",
+   Callback = function()
+   local Event = game:GetService("ReplicatedStorage").DamageHumanoid
+
+local oldNamecall
+oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+
+    if self == Event and method == "FireServer" then
+        print("weather tried to kill you, blocked it.")
+        return nil
+    end
+
+    return oldNamecall(self, ...)
+end))
+Rayfield:Notify({
+   Title = "Weather",
+   Content = "Won't take blizzard or rain damage.",
+   Duration = 3,
+   Image = 4483362458,
+})
+   end,
 })
